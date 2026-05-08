ab15c3cf14181513df6825d5f026ddc91c458857
thffkd <khayul0324@gmail.com>
2026-05-08 15:53:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ import pandas as pd
 st.set_page_config(page_title="남동고 등산", layout="wide")
 
 st.title("⛰️2026 학교 등산 행사 안내 지도⛰️")
-st.markdown("# 컴퓨터 동아리가 집적 만든 코스 가이드")
+st.markdown("컴퓨터 동아리가 집적 만든 코스 가이드입니다.")
 st.markdown("코스를 선택하고 행사에 참여해 보세요.")
+st.markdown("#큰 제목")
+st.markdown("##작은 제목")
+st.markdown("**굵은 글씨**")
+st.markdown("*이탤릭체*")
+
 st.text("집 가고 싶다")
