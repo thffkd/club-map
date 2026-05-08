cf00e29c28fe91ffde11baad37683cdf07dde8db
thffkd <khayul0324@gmail.com>
2026-05-08 16:17:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ df_latlon = df_latlon.rename(columns={'위도':'lat','경도':'lon'})
 # 3. 지도 생성 및 마커 표시(지도 시각화 단계)
 m = folium.Map(
     location=[37.40583317, 126.7214872],
-    zoom_start=15
+    zoom_start=12
 )
 
 folium.Marker(
@@ -40,3 +40,6 @@ folium.Marker(
   tooltip = "클릭해보세요",
   icon = folium.Icon(color='cadetblue', icon='info-sign')
 ).add_to(m)
+
+# 4. 화면 출력
+st_folium(m, width=700, height=500)
