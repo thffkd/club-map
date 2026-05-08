cdefceed42d4c1aa97dc31a1be3374600b799e60
thffkd <khayul0324@gmail.com>
2026-05-08 15:43:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# main.py
 import streamlit as st
+import folium
+from streamlit-folium import st_folium
+import pandas as pd
 
+# 1. 웹 페이지 설정
+st.set_page_config(page_title="남동고 등산", layout="wide")
 st.text("voi a ir a casa")
