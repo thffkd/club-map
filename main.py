9b602373198b8941126508d5c207f4caac3437ae
thffkd <khayul0324@gmail.com>
2026-05-08 15:32:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.text("mapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmapmap")
+st.text("voi a ir a casa")
