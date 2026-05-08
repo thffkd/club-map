78a0a2573aea397cac89f5aaf620a7e530dade61
thffkd <khayul0324@gmail.com>
2026-05-08 15:51:55 +0900

    Change markdown header level for course guide

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ import pandas as pd
 st.set_page_config(page_title="남동고 등산", layout="wide")
 
 st.title("⛰️2026 학교 등산 행사 안내 지도⛰️")
-st.markdown("## 컴퓨터 동아리가 집적 만든 코스 가이드")
+st.markdown("# 컴퓨터 동아리가 집적 만든 코스 가이드")
 st.markdown("코스를 선택하고 행사에 참여해 보세요.")
 st.text("집 가고 싶다")
